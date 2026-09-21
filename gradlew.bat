0b618dcacc0567bd371bffc7ed18cb3b9bee65fa
Bharath Pandi <bharathsvri@gmail.com>
2026-09-21 10:00:50 +0530

    remove the unwanted code

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set PATH=%PATH:"=%
-set GRADLE_BIN=C:\Users\m073\.gradle\wrapper\dists\gradle-9.5.0-bin\bvnork1r7n8i6kp5cnkibsc9q\gradle-9.5.0\bin\gradle.bat
-if exist "%GRADLE_BIN%" (
-    call "%GRADLE_BIN%" %*
-) else (
-    gradle %*
-)
